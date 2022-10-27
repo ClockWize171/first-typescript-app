02fbff44f6843c4fdb3c84071c64fa2c077d860e
ClockWize171 <jupiterminhtin998@gmail.com>
2022-10-27 20:30:59 +0630

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Note } from './models/note.model';
 import Header from './components/Header';
 import NoteLists from './components/NoteLists';
+import CreateNote from './components/CreateNote';
 import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
   const [notes, setNotes] = useState<Note[]>([{
-    id: (new Date()).toString(),
+    id: '2',
     title: "Meeting",
     text: 'Schedule Meeting with Team',
     color: '#dfdfdf',
     date: (new Date()).toString()
-  }]);
+  },
+  {
+    id: '1',
+    title: "Gaming",
+    text: 'Schedule with Game Developement team',
+    color: '#98e5d4',
+    date: (new Date()).toString()
+  }
+  ]);
   return (
     <>
       <Header />
@@ -21,6 +30,11 @@ function App() {
         <Row>
           <Col>
             <NoteLists notes={notes} setNotes={setNotes} />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <CreateNote notes={notes} setNotes={setNotes}/>
           </Col>
         </Row>
       </Container>
--- src/components/CreateNote.tsx
+++ src/components/CreateNote.tsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import * as React from 'react';
+import { Form, Button, Alert } from 'react-bootstrap';
+import { Note } from '../models/note.model';
 
-type Props = {}
-
-const CreateNote = (props: Props) => {
-  return (
-    <div>CreateNote</div>
-  )
+interface ICreateNoteProps {
+  notes: Note[],
+  setNotes: React.Dispatch<React.SetStateAction<Note[]>>
 }
 
-export default CreateNote
+const CreateNote: React.FunctionComponent<ICreateNoteProps> = ({ notes, setNotes }) => {
+  const [error, setError] = React.useState<string>("")
+
+  const titleRef = React.useRef<HTMLInputElement | null>(null);
+  const textRef = React.useRef<HTMLTextAreaElement | null>(null);
+  const colorRef = React.useRef<HTMLInputElement | null>(null);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    if (titleRef.current?.value === "" || textRef.current?.value === "") {
+      return setError("All fields are required!")
+    }
+    setError("")
+    setNotes([...notes, {
+      id: (new Date()).toString(),
+      title: (titleRef.current as HTMLInputElement).value,
+      text: (textRef.current as HTMLTextAreaElement).value,
+      color: (colorRef.current as HTMLInputElement).value,
+      date: (new Date()).toString(),
+    }]);
+
+    (titleRef.current as HTMLInputElement).value = "";
+    (textRef.current as HTMLTextAreaElement).value = "";
+  }
+
+  return (
+    <>
+      <h2>Create Note</h2>
+      {error && <Alert variant='danger'>{error}</Alert>}
+      <Form className='mt-3 mb-3' onSubmit={(e) => handleSubmit(e)}>
+        <Form.Group className='mb-3' controlId='formBasicTitle'>
+          <Form.Label>Title</Form.Label>
+          <Form.Control type='text' placeholder='Enter Title' ref={titleRef} />
+        </Form.Group>
+        <Form.Group className='mb-3' controlId='formBasicText'>
+          <Form.Label>Description</Form.Label>
+          <Form.Control as='textarea' placeholder='Enter description' ref={textRef} />
+        </Form.Group>
+        <Form.Group className='mb-3' controlId='formBasicTitle'>
+          <Form.Label htmlFor='colorInput'>Color</Form.Label>
+          <Form.Control type='color' id='colorInput' defaultValue='#dfdfdf' placeholder='Choose color' ref={colorRef} />
+        </Form.Group>
+        <Button type='submit' variant='primary'>Submit</Button>
+      </Form>
+    </>
+  );
+};
+
+export default CreateNote;
